db44aca33e336a85e2108737d9ddcb24580e6d26
Gil <g.f.hoben@gmail.com>
2018-03-29 18:59:30 +0100

    fixed directory name

--- devtools/travis-ci/install_libraries.sh
+++ devtools/travis-ci/install_libraries.sh
@@ -11,7 +11,7 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 150 --slave /
 # install boost 1.59 manually (this is the minimum requirement)
 wget -O boost_1_59_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.gz/download
 tar -xzf boost_1_59_0.tar.gz
-cd boost_1_66_0/
+cd boost_1_59_0/
 sudo apt-get install autotools-dev libicu-dev build-essential libbz2-dev -y
 ./bootstrap.sh --with-libraries=test
 ./b2 -j4
