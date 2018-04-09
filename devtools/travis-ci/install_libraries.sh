49964737e57ccddc42ff13bad435450db9f7fdc9
Gil <g.f.hoben@gmail.com>
2018-04-09 12:04:07 +0100

    boost 1.61 required for tests

--- devtools/travis-ci/install_libraries.sh
+++ devtools/travis-ci/install_libraries.sh
@@ -8,10 +8,10 @@ sudo apt-get install gcc-5 g++-5 autotools-dev libicu-dev build-essential libbz2
 # increase priority of gcc5 and g++5
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 150 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 
-# install boost 1.59 manually (this is the minimum requirement)
-wget -O boost_1_59_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.gz/download
-tar -xzf boost_1_59_0.tar.gz
-cd boost_1_59_0/
+# install boost 1.60 manually (this is the minimum requirement)
+wget -O boost_1_61_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz/download
+tar -xzf boost_1_61_0.tar.gz
+cd boost_1_61_0/
 ./bootstrap.sh --with-libraries=test
 ./b2 -j4
 sudo ./b2 install
--- prostruct/pdb/tests/PDBTestSuite.cpp
+++ prostruct/pdb/tests/PDBTestSuite.cpp
@@ -14,15 +14,12 @@ BOOST_AUTO_TEST_SUITE(PDBTests)
 
     BOOST_AUTO_TEST_CASE(LoadPDB) {
 
-        auto pdb = PDB("test.pdb");
-
-//        const char* L = "L";
-//        const char* H = "H";
+        PDB pdb = PDB("test.pdb");
 
         BOOST_TEST(pdb.n_chains() == 2);
 
-//        BOOST_TEST(pdb.getChainIDs()[0] == L);
-//        BOOST_TEST(pdb.getChainIDs()[1] == H);
+        BOOST_TEST(pdb.getChainIDs()[0] == "L");
+        BOOST_TEST(pdb.getChainIDs()[1] == "H");
 
         BOOST_TEST(pdb.getChain("H")->n_residues() == 123);
         BOOST_TEST(pdb.getChain("H")->n_atoms() == 974);
@@ -37,9 +34,9 @@ BOOST_AUTO_TEST_SUITE(PDBTests)
 
     BOOST_AUTO_TEST_CASE(KabschSander, *utf::tolerance(10e-9)) {
 
-        auto pdb = PDB("test.pdb");
+        PDB pdb = PDB("test.pdb");
 
-        auto E = pdb.calculate_KabschSander();
+        arma::mat E = pdb.calculate_KabschSander();
 
         BOOST_TEST(E.n_rows == pdb.n_residues());
         BOOST_TEST(E.n_cols == pdb.n_residues());
@@ -50,9 +47,9 @@ BOOST_AUTO_TEST_SUITE(PDBTests)
 
     BOOST_AUTO_TEST_CASE(PreditBackboneHBonds) {
 
-        auto pdb = PDB("test.pdb");
+        PDB pdb = PDB("test.pdb");
 
-        auto E = pdb.predict_backboneHbonds();
+        arma::mat E = pdb.predict_backboneHbonds();
 
         BOOST_TEST(E(2, 25) == 1);
 
