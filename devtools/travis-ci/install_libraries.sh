2ec4582e189744a77f0f1f7161faac6809186de2
Gil <g.f.hoben@gmail.com>
2018-04-10 09:16:32 +0100

    rolled back to 1.61 boost requirement

--- devtools/travis-ci/install_libraries.sh
+++ devtools/travis-ci/install_libraries.sh
@@ -8,9 +8,10 @@ sudo apt-get install gcc-5 g++-5 autotools-dev libicu-dev build-essential libbz2
 # increase priority of gcc5 and g++5
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 150 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 
-# install boost 1.67 manually (this is the minimum requirement)
-git clone --recursive https://github.com/boostorg/boost.git
-cd boost/
+# install boost 1.61 manually (this is the minimum requirement)
+wget -O boost_1_61_0.tar.gz https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz/download
+tar -xzf boost_1_61_0.tar.gz
+cd boost_1_61_0/
 ./bootstrap.sh --with-libraries=test
 ./b2 -j4
 cd ..
