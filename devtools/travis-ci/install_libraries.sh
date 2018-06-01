2aa985d902a2cc5c741fcf33f12ca99791fc03aa
Gil <g.f.hoben@gmail.com>
2018-06-01 11:53:55 +0100

    added blas and lapack to apt-get install

--- devtools/travis-ci/install_libraries.sh
+++ devtools/travis-ci/install_libraries.sh
@@ -4,7 +4,7 @@ cd $HOME
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -y
-sudo apt-get install gcc-5 g++-5 autotools-dev libicu-dev build-essential libbz2-dev -y
+sudo apt-get install gcc-5 g++-5 autotools-dev libicu-dev build-essential libbz2-dev libblas-dev liblapack-dev -y
 # increase priority of gcc5 and g++5
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 150 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 
