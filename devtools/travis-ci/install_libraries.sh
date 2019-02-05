775224dcadab099bc5b97f33934d89f83aaad58d
gf712 <gil_f.hoben@hotmail.com>
2019-02-05 14:59:58 +0000

    updated armadillo git clone

--- devtools/travis-ci/install_libraries.sh
+++ devtools/travis-ci/install_libraries.sh
@@ -9,7 +9,7 @@ sudo apt-get install gcc-5 g++-5 autotools-dev libicu-dev build-essential libbz2
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 150 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 
 # install armadillo 7.8 manually (trusty only has older versions)
-git clone -b 7.800.x https://github.com/conradsnicta/armadillo-code.git
+git clone -b 7.800.x https://gitlab.com/conradsnicta/armadillo-code.git
 cd armadillo-code/
 ./configure && make
 sudo make install
