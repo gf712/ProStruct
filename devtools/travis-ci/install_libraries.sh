0718fe8c59cb44f587d6269c947b07d8267ec66a
gf712 <gil_f.hoben@hotmail.com>
2019-03-08 08:51:52 +0000

    gcc fix

--- devtools/travis-ci/install_libraries.sh
+++ devtools/travis-ci/install_libraries.sh
@@ -4,9 +4,9 @@ cd $HOME
 
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update -y
-sudo apt-get install gcc-6 g++-6 autotools-dev libicu-dev build-essential libbz2-dev libblas-dev liblapack-dev -y
-# increase priority of gcc6 and g++6
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 150 --slave /usr/bin/g++ g++ /usr/bin/g++-6
+sudo apt-get install gcc-7 g++-7 autotools-dev libicu-dev build-essential libbz2-dev libblas-dev liblapack-dev -y
+# increase priority of gcc7 and g++7
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 150 --slave /usr/bin/g++ g++ /usr/bin/g++-7
 
 # install armadillo 7.8 manually (trusty only has older versions)
 git clone -b 7.800.x https://gitlab.com/conradsnicta/armadillo-code.git
